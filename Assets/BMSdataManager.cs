9b925db21c370cac9a847752be5abf1363a40282
Daru0122 <yoon65406@gmail.com>
2023-01-26 21:46:51 +0900

    2023-01-26
    아무리 생각해봐도 이 상태에서 변속을 만들기는 무리인 것 같음
    큰 맘먹고 싸그리 갈아엎음(파서 부분만 부분 수정 나머지 전부 제거)

--- Assets/BMSdataManager.cs
+++ Assets/BMSdataManager.cs
@@ -1,45 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using System.IO;
 using System.Text;
 
 
 public class BMSdataManager : MonoBehaviour
 {
-    public static KeyCode[] keyBinds = new KeyCode[9];
-    public bool[] inputActive = new bool[9];
-    public int combos;
-    public int score;
-    public static float[] judgeTimings = new float[5];
-    public List<Queue<GameObject>> notes = new List<Queue<GameObject>>();
-    public static Dictionary<string, string> BGA = new Dictionary<string, string>();
-    public static Dictionary<string, float> BPMs = new Dictionary<string, float>();
-    public static Dictionary<string, FMOD.Sound> WAV = new Dictionary<string, FMOD.Sound>();
-    public bool[] LNactive = new bool[9];
-    public float HI_SPEED;
-    public static string fileLoc = "C:/Program Files (x86)/Steam/steamapps/common/Qwilight/SavesDir/Bundle/Amnehilesie1108/Amnehilesie";//파일 위치 지정
-    public static float totalSCROLL;
-    float scrollPerJoint;
-    List<float> scalePerJoint = new List<float>();
-    [SerializeField] GameObject nManager;
-    [SerializeField] GameObject VideoPlayer;
-    public static System.Diagnostics.Stopwatch tT = new System.Diagnostics.Stopwatch();
-    int currentJoint;
+    //---------노트(배경음, 변속, bga, stop 등등 이벤트들도 모두 "노트"로서 처리함)------
+    //16진수, 36진수 디코더
+    //로딩에 필요한 변수들
+    private int noteType;
+    private string noteData;
     private char[] seps;
-    List<int> noteJoint = new List<int>();
-    List<int> noteData = new List<int>();
-    List<string> noteBeat = new List<string>();
+    private int currentMeasure;
+    //저장된 노트(아래 3개 리스트가 노트 하나를 이룸)
+    public List<float> noteBeats = new List<float>();
+    public List<int> noteTypes = new List<int>();
+    public List<float> noteValues = new List<float>();//float로 하려 했으나 bpm때문에..
+    //구역 크기는 데이터중 유일하게(아마도) 구역마다 처리되므로 따로처리
+    public List<float> scalePerMeasure = new List<float>();
+    //------------------------------------
+    public static Dictionary<string, string> BGA = new Dictionary<string, string>();//#BMPxx a 저장
+    public static Dictionary<string, float> BPMs = new Dictionary<string, float>();//#BPMxx a 저장
+    public static FMOD.Sound[] WAV = new FMOD.Sound[1296];//#WAVxx 파일 저장
+    public static string fileLoc = "C:/Program Files (x86)/Steam/steamapps/common/Qwilight/SavesDir/Bundle/Amnehilesie1108/Amnehilesie";//파일 위치 지정
+    public static System.Diagnostics.Stopwatch Time = new System.Diagnostics.Stopwatch();//초시계
+    //-------FMOD설정---------
     private FMOD.Sound snd;
     FMOD.System system;
     public static FMOD.Channel channel;
     public static FMOD.ChannelGroup channelGroup;
+    //------------------------
     //HEADER FILED 선언
     int PLAYER;//1:SP,2:Couple,3:DP
     string GENRE;
     string TITLE;
     string ARTIST;
-    public static float BPM;
+    public float BPM;
     int PLAYLEVEL;
     int RANK;//판정 난이도
 
@@ -54,51 +53,82 @@ public class BMSdataManager : MonoBehaviour
     //--------------------------------------------
     string input;
     string[] splitText;
-    public float loadDone;
+    float output;//함수 호출시 출력
 
     void Start(){
-        judgeTimings[0] = 40;
-        judgeTimings[1] = 80;
-        judgeTimings[2] = 120;
-        judgeTimings[3] = 15;
-        judgeTimings[4] = 150;
-        keyBinds[0] = KeyCode.S;
-        keyBinds[1] = KeyCode.D;
-        keyBinds[2] = KeyCode.F;
-        keyBinds[3] = KeyCode.Space;
-        keyBinds[4] = KeyCode.J;
-        keyBinds[5] = KeyCode.LeftShift;
-        keyBinds[7] = KeyCode.K;
-        keyBinds[8] = KeyCode.L;
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
-        notes.Add(new Queue<GameObject>());
         StartCoroutine(BMSload());
     }
-    void Update() {
-        totalSCROLL = tT.ElapsedMilliseconds*0.6f*BPM/174545;
+    float convertFromThirysix(string value){//두자리수만 가능
+        string[] convertToThirysix = new string[36]{
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+        };
+        output = (36*(Array.IndexOf(convertToThirysix, value.Substring(0,1))) + (Array.IndexOf(convertToThirysix, value.Substring(1,1))));
+        return output;
     }
-    IEnumerator play(){
-        yield return new WaitUntil(()=> loadDone >= 1);
-        tT.Start();
+    float convertFromSixteen(string value){//두자리수만 가능
+        string[] convertToThirysix = new string[16]{
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+        };
+        output = (16*(Array.IndexOf(convertToThirysix, value.Substring(0,1))) + (Array.IndexOf(convertToThirysix, value.Substring(1,1))));
+        return output;
     }
     IEnumerator BMSload(){
         yield return null;
         Debug.Log("로딩 시작");
-        for(int i = 0; i <= 8;){
-            LNactive[i] = false;
-            i++;
-        }
         system.createChannelGroup (null, out channelGroup);
         channel.setChannelGroup(channelGroup);
 
-        seps = new char[] {' ', ':'};//데이터 Splt 기준 정함
+        seps = new char[] {' ', ':'};//데이터 Split 기준 정함
         system = FMODUnity.RuntimeManager.CoreSystem;
         StreamReader reader = new StreamReader(new FileStream(fileLoc+"/amnehilasieSPN.bms", FileMode.Open), Encoding.GetEncoding("shift_jis"));
         //bms파일 판단하고 변수로 저장하는 과정
@@ -139,79 +169,47 @@ public class BMSdataManager : MonoBehaviour
                 }//-----------------------------WAV----------------------------
                 else if(input.Substring(1,3) == "WAV"){
                     system.createSound(fileLoc+"/"+input.Substring(input.IndexOf(" ")+1), FMOD.MODE.CREATESAMPLE | FMOD.MODE.ACCURATETIME, out snd);
-                    WAV.Add(input.Substring(4,2),snd);
+                    WAV[(int)output] = snd;
                 }//----------------------------BGA---------------------
                 else if(input.Substring(1,3) == "BMP"){
                     BGA.Add(input.Substring(4,2), input.Substring(input.IndexOf(' ')+1));
                 }//------------------------BPM---------------
                 else if(input.Substring(1,3) == "BPM"){
-                    BPMs.Add(input.Substring(4,2), float.Parse(input.Substring(input.IndexOf(' '))));
+                    BPMs.Add(input.Substring(4,2), float.Parse(splitText[1]));
                 }//-----------------------MAIN DATA FIELD----------------------
                 if(input.Substring(0,1) == "#" && input.Substring(6,1) == ":"){
-                    noteJoint.Add(int.Parse(input.Split(':')[0].Substring(1,3)));
-                    noteData.Add(int.Parse(input.Split(':')[0].Substring(4)));
-                    noteBeat.Add(input.Split(':')[1]);
+                    noteType = int.Parse(input.Split(':')[0].Substring(4));
+                    noteData = input.Split(':')[1];
+                    currentMeasure = int.Parse(input.Split(':')[0].Substring(1,3));
+                    if(noteType.Equals(1)){//배경음
+                        for(int i = 0; noteData.Length > i; i++){//2글자씩 읽음
+                            if(noteData.Substring(i*2,2) != "00"){//값이 0이 아닐때
+                                noteBeats.Add(currentMeasure+(i*noteData.Length/2));//저장
+                                noteTypes.Add(1);
+                                convertFromThirysix(noteData.Substring(i*2,2));
+                                noteValues.Add(output);
+                            }
+                        }
+                    }else if(noteType.Equals(2)){//마디별 크기
+                        while(scalePerMeasure.Count < int.Parse(input.Split(':')[0].Substring(1,3))){
+                            scalePerMeasure.Add(1);//빈마디는 1로 채워넣고
+                        }
+                        scalePerMeasure.Add(float.Parse(noteData));//현제 마디에 해당하는 값 넣기
+                    }else if(noteType.Equals(3)){//BPM변속(구식)
+                        for(int i = 0; noteData.Length > i; i++){
+                            if(noteData.Substring(i*2,2) != "00"){
+                                noteBeats.Add(currentMeasure+(i*noteData.Length/2));
+                                noteTypes.Add(8);//BPM변속(신식)으로 모두 변환
+                                convertFromSixteen(noteData.Substring(i*2,2));
+                                noteValues.Add(output);//옛날 BPM변속은 16진수(16*16-1=255)
+                            }
+                        }
+                    }else if(noteType.Equals(4)){
+                        
+                    }
                 }
             }
         }
         reader.Close();
-        //구역마다 크기를 미리 저장
-        for(int i = 0; noteJoint.Count > i;){
-            while(noteJoint.Count > i && currentJoint <= noteJoint[i]){
-                if(noteData[i].Equals(2)){
-                    while (scalePerJoint.Count < noteJoint[i]){
-                        scalePerJoint.Add(1);
-                    }
-                    scalePerJoint.Add(float.Parse(noteBeat[i]));
-                }
-                i++;
-            }
-            i++;
-        }
-        Debug.Log("완료");
-        StartCoroutine(startPerJoint());
-
-    }
-    IEnumerator startPerJoint(){
-        float SectionTime = 0f;
-        currentJoint = 0;
-        for(int i = 0; noteJoint.Count > i;){
-            if(currentJoint < noteJoint[i]){
-                //이 사이에 bpm변경 들어갈 예정
-                SectionTime += (240/BPM)*scalePerJoint[currentJoint];
-                scrollPerJoint += scalePerJoint[currentJoint]*144000/174545;
-                currentJoint++;
-            }
-            while(noteJoint.Count > i && currentJoint >= noteJoint[i]){
-                if(noteData[i]>10||noteData[i].Equals(1)){
-                    GameObject noteManager = Instantiate(nManager);
-                    NoteManager nManagerCom = noteManager.GetComponent<NoteManager>();
-                    nManagerCom.SecTime = SectionTime;
-                    nManagerCom.noteBeat = noteBeat[i];
-                    nManagerCom.secBPM = BPM;
-                    nManagerCom.secScroll = scrollPerJoint;
-                    nManagerCom.secScale = scalePerJoint[currentJoint];
-                    nManagerCom.secType = noteData[i];
-                    nManagerCom.loadDone = false;
-                    if(noteData[i]>50){
-                        nManagerCom.LNactive = LNactive[noteData[i]-51];
-                    }
-                    if(!noteData[i].Equals(1)){
-                        yield return new WaitUntil(()=> nManagerCom.loadDone);
-                        Destroy(noteManager);
-                    }
-                }else if(noteData[i].Equals(4)){
-                    playVideo playVideo = VideoPlayer.GetComponent<playVideo>();
-                    playVideo.data = noteBeat[i];
-                    playVideo.totalSecTime = SectionTime;
-                    playVideo.secTime = 240*scalePerJoint[i]/BPM;
-                    StartCoroutine(playVideo.play());
-                }
-                i++;
-            }
-            i++;
-        }
-        loadDone = 1;
-        StartCoroutine(play());
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RhythmGame
-Description
+http://hitkey.nekokan.dyndns.info/cmds.htm
+bms 포멧에 대해 여기서 알아봤음
